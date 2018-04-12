7e5ec7ce23222d7f4bdb6abbda7b9276a496c6b5
Logan Rice <loganrice@Logans-MacBook-Air-2.local>
2018-04-12 16:12:00 -0700

    added logo

--- src/components/SignInContainer.js
+++ src/components/SignInContainer.js
@@ -5,6 +5,8 @@ import EmailInput from './signin/EmailInput.js';
 import PasswordInput from './signin/PasswordInput.js';
 import store from '../store';
 import { signIn }  from '../actions/user-actions';
+import logo from '../logo.png';
+
 
 class SignInContainer extends Component {
   st() {
@@ -12,34 +14,35 @@ class SignInContainer extends Component {
   }
 
   render () {
-    let styles = { 
+    let containerStyle = { 
       height: '100vh',
       alignItems: 'center',
       textAlign: 'center'
     };
 
+    let logoStyle = {
+      height: '100px',
+      margin: '50px 17px'
+    }
+
     store.dispatch(signIn("user!!", "Password!!"))
     return (
-      <div className="content-grid mdl-grid" style={styles}>
+      <div className="content-grid mdl-grid" style={containerStyle}>
         <div className="mdl-layout-spacer"></div>
-
-        <div class=" mdl-card mdl-shadow--2dp">
-          <div class="mdl-card__title">
-            <h2 class="mdl-card__title-text">Login</h2>
+        <div className=" mdl-card mdl-shadow--2dp">
+          <img src={logo} className="App-logo" style={logoStyle} alt="logo" />
+          <div className="mdl-card__title">
+            <br />
+            <h2 className="mdl-card__title-text" style={{color: '#00c1b7'}}>Login</h2>
           </div>
-          <div onClick={this.st} class="mdl-card__supporting-text">
-            Make Collaborative Problem Solving Easier
-          </div>
-          <div class="mdl-card__actions mdl-card--border">
+          <div className="mdl-card__actions mdl-card--border">
             <form>
               <EmailInput /> 
               <PasswordInput />
               <SubmitButton  />
             </form>
-
           </div>
         </div>
-      
         <div className="mdl-layout-spacer"></div>
       </div>
     );
